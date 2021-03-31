743d55e176641969cb464cc75f4765d55a6195fc
xopxop <xopxop.1403@gmail.com>
2021-04-01 00:35:56 +0300

    test

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 00:25:51 by dthan             #+#    #+#             */
-/*   Updated: 2021/04/01 00:33:09 by dthan            ###   ########.fr       */
+/*   Updated: 2021/04/01 00:35:52 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
 	printf("Hello world!\n");
 	return (0);
