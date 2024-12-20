3d6537df9208e6225d24276be78055043a7f237a
Du Than <du.than@relexsolutions.com>
2024-12-20 16:16:40 +0200

    refined

--- lab/getrlimit.c
+++ lab/getrlimit.c
@@ -1,8 +1,10 @@
 #include <sys/resource.h>
+#include <stdio.h>
 
 int main()
 {
   struct rlimit r_limit;
   getrlimit(RLIMIT_CPU, &r_limit);
+  printf("Soft limit: %llu\n", r_limit.rlim_cur);
   return 0;
 }
